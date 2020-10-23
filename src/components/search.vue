89112d822e3995815bc1ed9bfd6d5255cb3cded2
Paul Bastowski <pbastowski@gmail.com>
2020-10-23 12:14:12 +0200

    dispatch search fetch after a debounce - don't debounce the typed characters, because that causes visual input lag

--- src/components/search.vue
+++ src/components/search.vue
@@ -6,12 +6,12 @@
             hide-details
             prepend-inner-icon="mdi-magnify"
             outlined
-            clearable
+            :clearable="grow"
             :style="{ 'max-width': grow ? '' : $vuetify.breakpoint.xs ? '70px' : '' }"
             :class="{ grow }"
             :rules="grow ? [allowedInputChars, minLength(3), maxLength(50)] : undefined"
-            :value="searchText"
-            v-on="{ input: debounce(v => updateSearchText(v), debounceDelay) }"
+            v-model="searchText"
+            @input="debounceSearch"
             @focus="grow = true"
             @blur="grow = !!searchText"
         />
@@ -23,11 +23,24 @@
     import debounce from 'lodash.debounce'
 
     export default {
-        data: () => ({ isValid: false, grow: false, debounceDelay: 600 }),
+        data: () => ({ isValid: false, grow: false }),
+
+        created() {
+            // Create a debounced method that calls fetchResults
+            // - when the user stops typing
+            // - and the input is valid, as per the rules prop on text-field
+            this.debounceSearch = debounce(v => this.isValid && this.fetchResults(v), 500)
+        },
 
         computed: {
-            searchText() {
-                return this.$store.state.searchText
+            // This is how you proxy a state from the store
+            searchText: {
+                get() {
+                    return this.$store.state.searchText
+                },
+                set(nv) {
+                    this.$store.state.searchText = nv
+                }
             }
         },
 
@@ -35,18 +48,10 @@
             allowedInputChars,
             maxLength,
             minLength,
-            debounce,
 
-            updateSearchText(value) {
-                value = value || ''
-                if (this.isValid) {
-                    this.$store.state.searchText = value
-                }
-            },
-
-            async fetchResults() {
-                await this.$store.dispatch('searchByText', this.searchText)
-                this.$router.push('/search-results')
+            async fetchResults(searchText) {
+                await this.$store.dispatch('searchByText', searchText)
+                this.$router.push('/shows')
             }
         }
     }
