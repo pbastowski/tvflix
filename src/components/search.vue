64653712016d6cef4c9a952571659689d8744bcd
Paul Bastowski <pbastowski@gmail.com>
2020-10-25 18:06:03 +0100

    remove console.logs

--- src/components/search.vue
+++ src/components/search.vue
@@ -65,14 +65,12 @@
                 if (isValid && searchText) {
                     this.$store.dispatch('searchByText', searchText)
                     this.$router.push(`/shows?q=${searchText}`)
-                    // console.log('SEARCH', searchText)
                 }
 
                 // If there is no text then we fetch popular shows
                 else if (!searchText) {
                     this.$store.dispatch('getPopularShows')
                     this.$router.push(`/shows`)
-                    // console.log('POPULAR')
                 }
             }
         }
