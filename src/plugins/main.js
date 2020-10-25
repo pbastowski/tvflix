cbb92dcc1fbf3c15a8a4c4345e7f857ae1c5024e
Paul Bastowski <pbastowski@gmail.com>
2020-10-25 11:06:58 +0100

    add pluralize function

--- src/plugins/main.js
+++ src/plugins/main.js
@@ -1,1 +1,5 @@
 import '@mdi/font/css/materialdesignicons.min.css'
+import Vue from 'vue'
+
+// A very generic pluralize - won't work for "sheep", for example, will for "car"
+Vue.prototype.pluralize = (text, count) => (count === 1 ? text : text + 's')
